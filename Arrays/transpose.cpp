d1fff6034a7ff34ba6e1e5ce5baf1f79a5e00af3
Aishani Singh <84021861+aishanii@users.noreply.github.com>
2022-02-22 18:37:56 +0530

    transpose of a 2-D matrix

---
+++ Arrays/transpose.cpp
@@ -0,0 +1,26 @@
+//to find transpose of a matrix
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+
+    for(int i=0;i<3;i++){
+        for(int j=i;j<3;j++){ //upper triangle
+            //swap
+            int temp=arr[i][j];
+            arr[i][j]=arr[j][i];
+            arr[j][i]=temp;
+
+        }
+    }
+
+    //print
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
